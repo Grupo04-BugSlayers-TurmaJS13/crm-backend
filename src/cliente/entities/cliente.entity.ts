066b0a6c755f2a751c7bb6397b5ce337d528dc96
clarisse <clarissebleasby@gmail.com>
2026-03-12 12:41:28 -0300

    ✨ New Feature: Construçao da classe Clientes

---
+++ src/cliente/entities/cliente.entity.ts
@@ -0,0 +1,35 @@
+import { Transform, TransformFnParams } from "class-transformer"
+import { IsEmail, IsNotEmpty } from "class-validator"
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+
+@Entity({name: 'tb_clientes'})
+
+export class Cliente {
+    @PrimaryGeneratedColumn() 
+    id: number
+
+    @Transform(({ value } : TransformFnParams) => value?.trim()) 
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false}) 
+    nome: string
+
+    @Transform(({ value } : TransformFnParams) => value?.trim()) 
+    @IsEmail()
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false}) 
+    email: string
+
+    @Transform(({ value } : TransformFnParams) => value?.trim()) 
+    @IsNotEmpty()
+    @Column({length: 20, nullable: false}) 
+    telefone: string
+
+    @Transform(({ value } : TransformFnParams) => value?.trim()) 
+    @Column({length: 500}) 
+    sites: string
+
+    // @OneToMany(() => Oportunidade, (oportunidade) => oportunidade.cliente)
+    //oportunidades: Oportunidade[]
+
+}
