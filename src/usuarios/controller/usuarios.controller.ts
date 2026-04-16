011c75a44e06003132602974d7b6334447cf50de
SabrinaNovaes <sabrinanovaes96@gmail.com>
2026-04-16 13:19:02 -0300

    🐛 FIX:

--- src/usuarios/controller/usuarios.controller.ts
+++ src/usuarios/controller/usuarios.controller.ts
@@ -30,15 +30,15 @@ export class UsuariosController {
     return this.service.listarUsuarios();
   }
 
-  @Get('/:id')
-  @HttpCode(HttpStatus.OK)
-  listarUsuariosId(@Param('id', ParseIntPipe) id: number) {
-    return this.service.listarUsuariosId(id);
-  }
-
   @Get('por-nome')
   @HttpCode(HttpStatus.OK)
   listarPorNome(@Query('nome') nome: string) {
     return this.service.listarPorNome(nome);
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  listarUsuariosId(@Param('id', ParseIntPipe) id: number) {
+    return this.service.listarUsuariosId(id);
   }
 }
