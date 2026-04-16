5ce73d2fd52e081b790936fb594300a747afc4d1
SabrinaNovaes <sabrinanovaes96@gmail.com>
2026-04-16 12:00:34 -0300

    💥 NEW:  Configuração do Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Conecta CRM')
+  .setDescription('Projeto Conecta CRM')
+  .setContact("BugSlayers","https://github.com/BugSlayers-tjs13","grupo.04.turma.javascript.13@gmail.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
 //para validar biblioteca
 app.useGlobalPipes(new ValidationPipe());
